3c8806d6e8feab30e6dbd192ce4cd268404b0aa9
José Iriel de Freitas Martins <jose.iriel.martins@gmail.com>
2024-05-07 17:30:28 -0300

    Refatora o método principal

--- src/Main.java
+++ src/Main.java
@@ -9,20 +9,25 @@ public class Main {
     static Scanner inputScanner = new Scanner(System.in);
     public static final String chaveApi = System.getenv("CHAVE");
 
+    static Currency pedirMoeda(String mensagem) {
+        System.out.print(mensagem);
+        return Currency.getInstance(inputScanner.nextLine().toUpperCase());
+    }
+
+    static double pedirDouble(String mensagem) {
+        System.out.print(mensagem);
+        return inputScanner.nextDouble();
+    }
+
     public static void main(String[] args) throws IOException, InterruptedException {
         System.out.printf("%s v%s\n", nome, versao);
         System.out.println("Copyright 2024 José Martins\n");
 
-        System.out.print("Digite o código ISO 4217 (ex.: USD/BRL/MXN) da moeda DE que quer converter: ");
-        var moedaOriginal = Currency.getInstance(inputScanner.nextLine().toUpperCase());
-
-        System.out.print("Digite o código da moeda PARA a qual quer converter: ");
-        var moedaConvertida = Currency.getInstance(inputScanner.nextLine().toUpperCase());
-
-        System.out.print("Digite o valor a ser convertido: ");
-        var valor = inputScanner.nextDouble();
-
+        var moedaOriginal = pedirMoeda("Digite o código ISO 4217 (ex.: USD/BRL/MXN) da moeda DE que quer converter: ");
+        var moedaConvertida = pedirMoeda("Digite o código da moeda PARA a qual quer converter: ");
+        var valor = pedirDouble("Digite o valor a ser convertido: ");
         var conversor = new Conversor(chaveApi, moedaOriginal, moedaConvertida);
+
         System.out.printf("O valor convertido é: %.2f\n", conversor.converter(valor));
     }
 }
